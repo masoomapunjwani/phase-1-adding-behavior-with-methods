e07b89a4856e54a3fb2825a49727e12966d9e6a2
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-09-08 13:34:02 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says meow!`;
+    }
+}
+
+const cat = new Cat("rose", "female");
+cat;
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return  `${this.name} says woof!`;
+    }
+}
+
+const dog = new Dog("harry", "male");
+dog;
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        if(this.sex === "male") {
+            return `It's me! ${this.name}, the parrot!`;
+        }else {
+            return `${this.name} says squawk!`;
+        }
+    }
+}
+
+const bird = new Bird("koo", "male");
+bird;
